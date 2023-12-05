1c743c19dd5d4ecf2b6deb90b16451b9af7cb19a
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2023-12-05 17:03:56 +0100

    Add files via upload

---
+++ S7_L2_esercitazione/index.js
@@ -0,0 +1,66 @@
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  static isSameAge(utenteA, utenteB) {
+    return utenteA.age > utenteB.age
+      ? "l'utente " + utenteA.firstName + " è più grande di " + utenteB.firstName
+      : "l'utente " + utenteA.firstName + " è più piccolo o ha la stessa età di " + utenteB.firstName;
+  }
+}
+
+const utente1 = new User("Mario", "Rossi", 80, "Betlemme");
+console.log(utente1);
+const utente2 = new User("Germano", "Mosconi", 33, "Paradiso");
+console.log(utente2);
+console.log(User.isSameAge(utente1, utente2));
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  hasSameOwner(otherPet) {
+    return this.ownerName === otherPet.ownerName;
+  }
+}
+
+function createPet() {
+  const petName = document.getElementById("petName").value;
+  const ownerName = document.getElementById("ownerName").value;
+  const species = document.getElementById("species").value;
+  const breed = document.getElementById("breed").value;
+
+  const newPet = new Pet(petName, ownerName, species, breed);
+  displayPet(newPet);
+}
+
+function displayPet(pet) {
+  const petList = document.getElementById("petList");
+  const listItem = document.createElement("li");
+  listItem.textContent = `Pet Name: ${pet.petName}, Owner Name: ${pet.ownerName}, Species: ${pet.species}, Breed: ${pet.breed}`;
+  petList.appendChild(listItem);
+
+  let sharesOwner = false;
+  for (let i = 0; i < petList.children.length - 1; i++) {
+    const previousPet = petList.children[i].petObject;
+    if (pet.hasSameOwner(previousPet)) {
+      sharesOwner = true;
+      break;
+    }
+  }
+
+  if (sharesOwner) {
+    listItem.textContent += " (Questo padrone ha un altro animale)";
+  }
+
+  // Attach the pet object to the list item for comparison in the future
+  listItem.petObject = pet;
+}
